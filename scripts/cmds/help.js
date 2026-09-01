1e70b09e435790213b3ab8b3ff0d2a58a40ee06d
hsakkaak <ghukha011@gmail.com>
2026-09-01 11:36:04 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -63,7 +63,7 @@ module.exports = {
                                        `Type ${prefix}help <cmd> to see details.`;
 
                         msg += `\n\n⭔ Total Commands: ${totalCommands}\n⭔ ${helpHint}\n`;
-                        msg += `\n╭─✦ ADMIN: MahMUD 彡\n├‣ WHATSAPP\n╰‣ 01836298139`;
+                        msg += `\n╭─✦ ADMIN: SHAAHLOM彡\n├‣ WHATSAPP\n╰‣ 01794197687`;
 
                         try {
                                 const hh = await message.reply({ body: msg });
